c8840de037aed3f2f42aa52ff2128820003bff53
lisaula <luisca_elfather@hotmail.com>
2016-09-14 16:48:03 -0600

    Finished without SOLID

--- src/com/solidgame/CommandFiles.java
+++ src/com/solidgame/CommandFiles.java
@@ -1,28 +1,20 @@
 package com.solidgame;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.util.ArrayList;
 
 public class CommandFiles {
-	private FileReader rComandos;
-	//public ArrayList<String> comandos;
-
-	public CommandFiles() throws IOException{
-		rComandos = new FileReader("test.scmd");
-	}
 
 	public ArrayList<String> getCommandos() throws IOException{
 		ArrayList<String> comandos = new ArrayList<>();
 		
-		FileReader f = new FileReader("test.scmd");
-		BufferedReader bf = new BufferedReader(f);
+		FileReader file = new FileReader("test.scmd");
+		BufferedReader buffer = new BufferedReader(file);
 		String cadena;
 
-		while ((cadena=bf.readLine()) != null) {
+		while ((cadena=buffer.readLine()) != null) {
 			comandos.add(cadena);
 		}
 
--- src/com/solidgame/SolidGame.java
+++ src/com/solidgame/SolidGame.java
@@ -27,36 +27,77 @@ public class SolidGame {
 			System.out.println(board[i]);			
 		}
 	}
+        public void updatePosition(String step){
+            switch (step){
+                case "arriba":
+                    row--;
+                    break;
+                case "abajo":
+                    row++;
+                    break;
+                case "izquierda":
+                    column--;
+                    break;
+                case "derecha":
+                    column++;
+                    break;
+                default:
+                    System.out.println("Command invalid");
+            }
+        }
+        public boolean validateWinner(){
+            if(board[row][column] == 'F')
+                return true;
+            return false;
+        }
+        
+        public void move(){
+            if(commands.size()>0){
+                if(validateMove(commands.get(0))){
+                    updatePosition(commands.get(0));
+                    System.out.println("You've moved: "+commands.get(0));
+                    commands.remove(0);
+                    if(validateWinner()){
+                        System.out.println("...You Won...");
+                    }else{
+                        move();
+                    }
+                }else{
+                    System.out.println("Game over\nInvalid Position");
+                }
+            }else
+                System.out.println("Game Over");
+        }
 	
 	public boolean validateMove(String step){
 		switch(step){
 			case "arriba":
-				if(row>0){
-					if(board[row-1][column]!='P'){
-						return true;
-					}
-				}
+                            if(row>0){
+                                    if(board[row-1][column]!='P'){
+                                            return true;
+                                    }
+                            }
 			break;
 			case "abajo":
-				if(row<board.length-1){
-					if(board[row+1][column]!='P'){
-						return true;
-					}
-				}
+                            if(row<board.length-1){
+                                    if(board[row+1][column]!='P'){
+                                            return true;
+                                    }
+                            }
 			break;
 			case "derecha":
-				if(column<board[0].length-1){
-					if(board[row][column+1]!='P'){
-						return true;
-					}
-				}
+                            if(column<board[0].length-1){
+                                    if(board[row][column+1]!='P'){
+                                            return true;
+                                    }
+                            }
 			break;
 			case "izquierda":
-				if(column>0){
-					if(board[row][column-1]!='P'){
-						return true;
-					}
-				}
+                            if(column>0){
+                                    if(board[row][column-1]!='P'){
+                                            return true;
+                                    }
+                            }
 			break;
 		}
 		
@@ -69,6 +110,6 @@ public class SolidGame {
 			System.out.println(game.commands.get(i));
 			System.out.println(game.validateMove(game.commands.get(i)));
 		}
-		
+		game.move();
 	}
 }
